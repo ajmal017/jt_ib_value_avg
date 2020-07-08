e97bbee7d1ca017659bfdc3696c76b68d365cacb
Matthew Cross <mattyc246@gmail.com>
2020-07-08 18:56:47 +0800

    added comments to app

--- app.py
+++ app.py
@@ -23,3 +23,14 @@ def main():
 
 if __name__ == "__main__":
     print(main())
+
+"""
+Comments:
+
+1. app.connect() creates a connection between the IB Gateway API and script
+2. app.run() is REQUIRED to be called in order to start the connection
+3. nextValidId is first overwritten function to get called in the IBApi file
+4. Inside nextValidId, the start function is called
+5. Most of the request functions are asynchronous, the data does not necessarily get returned immediately
+6. To intercept the response from the API, the functions marked with @overwrite catch the response and handle it
+"""
